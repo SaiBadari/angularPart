51317e3c7753aa694d72a499c35fbd638a7d7807
SaiBadari <sai.badari98@gmail.com>
2020-05-06 13:13:21 +0530

    second commit

---
+++ src/app/student/student.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { StudentservService } from '../studentserv.service';
+
+@Component({
+  selector: 'app-student',
+  templateUrl: './student.component.html',
+  styleUrls: ['./student.component.css']
+})
+export class StudentComponent implements OnInit {
+
+  constructor(private studentserv: StudentservService) { }
+
+  ngOnInit(): void {
+  }
+
+}
